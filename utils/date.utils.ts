769589ac54335e4b415e407228ec47ca4827485b
Ivan Grekov <ivan.grekov@windmill.digital>
2023-06-20 20:39:32 +0300

    utils were removed

--- utils/date.utils.ts
+++
@@ -1,7 +0,0 @@
-export const getMonthName = (date: Date): string => {
-    return date.toLocaleString('default', { month: 'long' });
-}
-
-export const getYear = (date: Date): number => {
-    return date.getFullYear();
-}
