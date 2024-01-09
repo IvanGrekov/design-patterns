8aa116667eb521635995d882ecbfe54a5e2e8844
Ivan Grekov <ivan.grekov@windmill.digital>
2024-01-09 21:20:25 +0200

    decorator

--- structutal-patterns/decorator.ts
+++ structutal-patterns/decorator.ts
@@ -1,29 +1,43 @@
-interface IDeveloper {
+export interface IDeveloper {
     language: string;
     work(): void;
 }
 
-class TraineeDeveloper implements IDeveloper {
-    constructor(public language: string) {}
+class Developer implements IDeveloper {
+    constructor(public language: string, protected salary: number) {}
 
     public work() {
-        console.log(`I'm working with ${this.language}`);
-    }
-}
-
-class Developer extends TraineeDeveloper {
-    constructor(public language: string, protected salary: number) {
-        super(language);
-    }
-
-    public work() {
-        super.work();
         console.log(`I'm working for ${this.salary}`);
+        console.log(`I'm writing code in ${this.language}`);
     }
 }
 
-const traineeDeveloper = new TraineeDeveloper('JavaScript');
-const developer = new Developer('TypeScript', 1000);
+class PoliteDeveloper implements IDeveloper {
+    constructor(protected developer: Developer) {}
 
-traineeDeveloper.work();
+    public get language() {
+        return this.developer.language;
+    }
+
+    public work() {
+        console.log('Hello, I am a polite developer');
+        this.developer.work();
+    }
+}
+
+const developer = new Developer('TypeScript', 1000);
+const developer1 = Object.create(
+    Developer.prototype,
+    {
+        language: { value: 'JavaScript' },
+        salary: { value: 2000 },
+    }
+);
+const politeDeveloper = new PoliteDeveloper(developer);
+
 developer.work();
+console.log('------------------');
+developer1.work();
+console.log('------------------');
+politeDeveloper.work();
+
